1a7050c3e13f7540f10040fc05713f41221e0c99
dendy-senapartha <wb-ikadekden434525>
2019-08-22 14:46:00 +0700

    + add registry and gateway microservice

---
+++ non-functional/registry-service/src/main/java/com/microservice/registry/ServiceRegistryMain.java
@@ -0,0 +1,16 @@
+package com.microservice.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryMain {
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryMain.class, args);
+	}
+}
